c51b7374ff0e7cd89d364e3a18d07b5719c3830b
kunal-2668 <kunal0.kr@gmail.com>
2022-07-14 13:22:21 +0530

    makeitbetter

--- crud_pro/settings.py
+++ crud_pro/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR/'static').
+STATIC_ROOT = os.path.join(BASE_DIR/'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
